69d0af07db0a8f320b3a47085dfa06cf83334034
Vineet Tiruvadi <virati@veritexx.net>
2017-04-12 18:20:35 -0400

    more modifications to enable frequency window setting

--- Frame_Load.py
+++ Frame_Load.py
@@ -52,19 +52,21 @@ f_trunc = np.where(np.logical_and(f_vect < f_focus[1], f_vect > f_focus[0]))[0]
 f_dict = {'FreqVector': f_vect, 'FreqTrunc': f_trunc}
 
 #fixed 3test x 3train approach (for now)
-training_patients = ['DBS905','DBS907','DBS906']
-test_patients = ['DBS901','DBS903','DBS908']
+training_patients = ['905','907','906']
+test_patients = ['901','903','908']
 
 #Need to make our design matrix now
 #get the list of ephys-related clinical phases
-def Phase_Matrix(DataFrame,Phases):    
+def Phase_Matrix(DataFrame,Phases,max_freq = 211,training_patients=['901','903','905','906','907','908']):
+    max_fidx = int(max_freq/211 * 512)
+    
     dsgnX = []
     dsgnY = []
-    for pt, patient in enumerate(['901','903','905','906','907','908']):
+    for pt, patient in enumerate(training_patients):
         for pp,phase in enumerate(DataFrame['DBS'+patient].keys()):
             if phase[0] != 'A':
                 try:
-                    state_vector = np.hstack((DataFrame['DBS'+patient][phase]['MeanPSD']['LOGPxx'][:512,0],DataFrame['DBS'+patient][phase]['MeanPSD']['LOGPxx'][:,1]))
+                    state_vector = np.hstack((DataFrame['DBS'+patient][phase]['MeanPSD']['LOGPxx'][:max_fidx,0],DataFrame['DBS'+patient][phase]['MeanPSD']['LOGPxx'][:max_fidx,1]))
                     dsgnX.append(state_vector)
                     dsgnY.append(DataFrame['DBS'+patient][phase]['HDRS17'])
                 except:
@@ -77,10 +79,10 @@ def Phase_Matrix(DataFrame,Phases):
     #return the design tensors and flags for NaN presence
     return Xout, Yout
     
-DsgnX, clinY = Phase_Matrix(DataFrame,Phase_List(exprs='ephys'))
+DsgnX, clinY = Phase_Matrix(DataFrame,Phase_List(exprs='ephys'),max_freq=70,training_patients=training_patients)
 
 #The actual regression code below
-EN_alpha = 0.05
+EN_alpha = 0.2
 #Generate our elastic net model now
 DM = ElasticNet(alpha=EN_alpha,tol=0.001,normalize=True,positive=False)
 DM.fit(DsgnX,clinY)
