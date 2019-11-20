823c129610c5b114c386205eb58522471309dd87
Vineet Tiruvadi <virati@gmail.com>
2019-11-20 17:39:08 -0500

    Updating trad analysis and adding min-max HDRS analysis

--- TraditAnalysis_hdrs.py
+++ TraditAnalysis_hdrs.py
@@ -4,7 +4,7 @@
 Created on Wed Feb  7 09:24:26 2018
 
 @author: virati
-Traditional Analysis, comparing two timepoints
+Traditional Analysis, comparing extremes of HDRS17
 """
 
 from DBSpace.readout.BR_DataFrame import BR_Data_Tree
@@ -18,6 +18,7 @@ import sys
 #sys.path.append('/home/virati/Dropbox/projects/Research/MDD-DBS/Ephys/SigProc/CFC-Testing/Python CFC/')
 import DBSpace as dbo
 from DBSpace import nestdict
+from DBSpace.readout import ClinVect
 
 import seaborn as sns
 #sns.set_context("paper")
@@ -31,38 +32,38 @@ import pickle
 from matplotlib.patches import Rectangle, Circle
 from numpy import ndenumerate
 
+## Parameters for the analysis
+ks_stats = nestdict()
+pts = ['901','903','905','906','907','908']
+bands = ['Delta','Theta','Alpha','Beta*','Gamma1']
+all_feats = ['L-' + band for band in bands] + ['R-' + band for band in bands]
+
+
+# for each patient, let's find the highest and lowest HDRS17 value and the week we find it
+ClinFrame = ClinVect.CFrame(norm_scales=True)
+
+hdrs_info = nestdict()
+
+for pt in pts:
+    pt_hdrs_traj = [a for a in ClinFrame.DSS_dict['DBS'+pt]['HDRS17']]
+    hdrs_info[pt]['max']['week'] = np.argmax(pt_hdrs_traj)
+    hdrs_info[pt]['min']['week'] = np.argmin(pt_hdrs_traj)
+    hdrs_info[pt]['max']['HDRS'] = pt_hdrs_traj[hdrs_info[pt]['max']['week']]
+    hdrs_info[pt]['min']['HDRS'] = pt_hdrs_traj[hdrs_info[pt]['min']['week']]
+
 
 #%%
 BRFrame = pickle.load(open('/home/virati/Chronic_Frame.pickle',"rb"))
-#BRFrame.full_sequence(data_path='/home/virati/Dropbox/Chronic_Frame_June.npy')
-
 #do a check to see if any PSDs are entirely zero; bad sign
 BRFrame.check_meta()
 
 
-#%%
 #Move forward with traditional oscillatory band analysis
 from OBands import *
 analysis = OBands(BRFrame)
 analysis.feat_extract(do_corrections=True)
 
-##PLOTS
-
-#focus_feats =['Delta','Theta','Alpha','Beta']
-#Now we're ready for the plots
 #%%
-#First thing is the per-patient weekly averages plotted for left and right
-#_ = analysis.mean_psds(weeks=["C01","C24"],patients='all')
-
-
-#%%
-# Do comparison of two timepoints here
-#analysis.scatter_state(week='all',pt=['908'],feat='SHarm')
-#analysis.scatter_state(week='all',pt=['908'],feat='Stim')
-ks_stats = nestdict()
-pts = ['901','903','905','906','907','908']
-bands = ['Delta','Theta','Alpha','Beta*','Gamma1']
-all_feats = ['L-' + band for band in bands] + ['R-' + band for band in bands]
 
 circ = 'day'
 week_distr = nestdict()
