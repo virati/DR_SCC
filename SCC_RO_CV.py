3e5788e65cb976e5fa2f2a0b38748c9623922c57
Vineet Tiruvadi <virati@gmail.com>
2020-05-29 15:48:01 -0400

    Updates to SCC_RO and CV version, close to end for the paper

--- SCC_RO_CV.py
+++ SCC_RO_CV.py
@@ -24,17 +24,10 @@ import matplotlib.pyplot as plt
 plt.rcParams['image.cmap'] = 'tab10'
 plt.close('all')
 
-#
+
 ## MAJOR PARAMETERS for our partial biometric analysis
 do_pts = ['901','903','905','906','907','908'] # Which patients do we want to include in this entire analysis?
 test_scale = 'pHDRS17' # Which scale are we using as the measurement of the depression state?
-
-''' DETRENDING
-Which detrending scheme are we doing
-This is important. Block goes into each patient and does zero-mean and linear detrend across time
-None does not do this
-All does a linear detrend across all concatenated observations. This is dumb and should not be done. Will eliminate this since it makes no sense
-'''
 
 # Initial
 # Now we set up our DBSpace environment
@@ -44,12 +37,14 @@ ClinFrame = ClinVect.CStruct()
 BRFrame = pickle.load(open('/home/virati/Dropbox/Data/Chronic_FrameMay2020.pickle',"rb"))
 
 #%%
-main_readout = decoder.weekly_decoderCV(BRFrame=BRFrame,ClinFrame=ClinFrame,pts=do_pts,clin_measure=test_scale,algo='ENR')
+main_readout = decoder.weekly_decoderCV(BRFrame=BRFrame,ClinFrame=ClinFrame,pts=do_pts,clin_measure=test_scale,algo='ENR',alpha=-4,shuffle_null=False) #main analysis is -3.4
+main_readout.global_plotting = True
 main_readout.filter_recs(rec_class='main_study')
 main_readout.split_train_set(0.6)
 
 #%%
 main_readout.train_setup()
+optimal_alpha = main_readout._path_slope_regression()
 main_readout.train_model()
 #%%
 main_readout.plot_decode_CV()
@@ -57,13 +52,14 @@ main_readout.plot_decode_CV()
 #%%
 main_readout.test_setup()
 main_readout.test_model()
-#%%0
+
+main_readout.plot_test_timecourse()
+#%%
 main_readout.plot_test_stats()
 #%%
 main_readout.plot_test_regression_figure()
 #main_readout.plot_combo_paths()
-main_readout._path_slope_regression()
 #%%
 # Now we move on to the classifier analysis
-threshold_c = decoder.controller_analysis(main_readout)
+threshold_c = decoder.controller_analysis(main_readout,bin_type='threshold')
 threshold_c.classif_runs()
