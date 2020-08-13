6b51c1922ea212a71b318b09bae1a14fa06e4954
Vineet Tiruvadi <virati@gmail.com>
2020-08-12 22:10:02 -0400

    Updates to the readout that should be small and inconsequential

--- TraditAnalysis_hdrs.py
+++ TraditAnalysis_hdrs.py
@@ -67,7 +67,7 @@ BRFrame.check_meta()
 #Move forward with traditional oscillatory band analysis
 from OBands import *
 feat_frame = OBands(BRFrame)
-feat_frame.feat_extract(do_corrections=False)
+feat_frame.feat_extract(do_corrections=True)
 
 
 #main_readout = naive_readout(feat_frame,ClinFrame)
@@ -80,10 +80,17 @@ sig_stats = nestdict()
 for pt in pts:
     for ff in bands:
         # if we want to compare max vs min hdrs
-        #feats,sig_stats,week_distr[pt][ff] = feat_frame.compare_states([hdrs_info[pt]['max']['week'],hdrs_info[pt]['min']['week']],feat=ff,circ='day',stat='ks')
+        feats,sig_stats[pt][ff],week_distr[pt][ff] = feat_frame.compare_states([hdrs_info[pt]['max']['week'],hdrs_info[pt]['min']['week']],feat=ff,circ='day',stat='ks')
         # if we want to compare early vs late
-        feats,sig_stats[pt][ff],week_distr[pt][ff] = feat_frame.compare_states(["C01","C24"],pt=pt,feat=ff,circ='day',stat='ks')
+        #feats,sig_stats[pt][ff],week_distr[pt][ff] = feat_frame.compare_states(["C01","C24"],pt=pt,feat=ff,circ='day',stat='ks')
     
+
+# Make our big stats 2d grid for all features across all patients
+K_stats = np.array([[[sig_stats[pt][band][side][0] for side in ['Left','Right']] for band in bands] for pt in pts]).reshape(6,-1,order='F')
+P_val = np.array([[[sig_stats[pt][band][side][1] for side in ['Left','Right']] for band in bands] for pt in pts]).reshape(6,-1,order='F')
+pre_feat_vals = np.array([[[week_distr[pt][band][side]['depr'] for side in ['Left','Right']] for band in bands] for pt in pts]).reshape(6,-1,order='F')
+post_feat_vals = np.array([[[week_distr[pt][band][side]['notdepr'] for side in ['Left','Right']] for band in bands] for pt in pts]).reshape(6,-1,order='F')
+
 
 depr_list = []
 notdepr_list = []
@@ -121,4 +128,42 @@ ax = sns.violinplot(data=depr_list,color='blue')
 ax = sns.violinplot(data=notdepr_list,color='red',alpha=0.3)
 _ = plt.setp(ax.collections,alpha=0.3)
 
-#%%
+#%% GRID GETS PLOTTED HERE
+def get_distr_change(patient,band,plot=True):
+    
+    pp = pts.index(patient)
+    ff = all_feats.index(band)
+    if plot:
+        plt.figure()
+        sns.violinplot(y=pre_feat_vals[pp,ff])
+        sns.violinplot(y=post_feat_vals[pp,ff],color='red',alpha=0.3)
+
+    return (np.mean(post_feat_vals[pp,ff]) - np.mean(pre_feat_vals[pp,ff]))
+
+
+change_grid = np.zeros((len(pts),len(all_feats)))
+for pp,pt in enumerate(pts):
+    for ff,freq in enumerate(all_feats):
+        change_grid[pp,ff] = get_distr_change(pt,freq,plot=False)
+        
+plt.figure()
+plt.pcolormesh(change_grid,cmap=plt.cm.get_cmap('viridis'))
+plt.colorbar()
+plt.xticks(np.arange(10)+0.5,bands + bands,rotation=90)
+plt.yticks(np.arange(6)+0.5,pts)
+plt.title('Band Change - High -> Low HDRS')
+
+ax = plt.axes()
+
+for index,value in ndenumerate(P_val):
+    if value < (0.05/10):
+        if change_grid[index] > 0:
+            usecolor='red'
+        else:
+            usecolor='blue'
+            
+        print(index)
+        ax.add_patch(Rectangle((index[1], index[0]), 1, 1, fill=False, edgecolor='red', lw=5))
+        ax.add_patch(Circle((index[1]+0.5, index[0]+0.5), 0.2, fill=True, facecolor=usecolor, edgecolor='white', lw=2))
+plt.show()
+
